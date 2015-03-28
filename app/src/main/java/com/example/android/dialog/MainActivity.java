5f13be2a57dac81187b116c4f7b2c7d16f65ce70
labnova <progettofeb@gmail.com>
2015-03-28 01:35:43 +0100

    add two more complex example of ProgressDialog using Thread

--- app/src/main/java/com/example/android/dialog/MainActivity.java
+++ app/src/main/java/com/example/android/dialog/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -14,6 +15,7 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
     CharSequence[] items = {"Google", "Apple", "Microsoft"};
     boolean[] itemsChecked = new boolean[items.length];
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,46 @@ public class MainActivity extends ActionBarActivity {
 
     public void onClick(View v) {
         showDialog(0);
+    }
+
+    public void onClick2(View v) {
+        final ProgressDialog dialog = ProgressDialog.show(this, "Fai qualcosa", "Aspetta", true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //simulare qualcosa di lungo...
+                    Thread.sleep(5000);
+                    //dismettere il dialog
+                    dialog.dismiss();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void onClick3(View v) {
+        showDialog(1);
+        progressDialog.setProgress(0);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=1; i<=15; i++) {
+                    try {
+                        //simulare qualcosa di lungo...
+                        Thread.sleep(1000);
+                        //update del dialog...
+                        progressDialog.incrementProgressBy((int)(100/15));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                progressDialog.dismiss();
+            }
+        }).start();
     }
 
     @Override
@@ -56,6 +98,26 @@ public class MainActivity extends ActionBarActivity {
                                                 Toast.LENGTH_SHORT).show();
                                     }
                                 }).create();
+            case 1:
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setIcon(R.drawable.abc_btn_radio_material);
+                progressDialog.setTitle("Download di qualcosa...");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getBaseContext(), "OK cliccato!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getBaseContext(), "Cancel cliccato!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                return progressDialog;
         }
 
         return null;
